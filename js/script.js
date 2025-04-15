3db12303f6ac0d9c2df022b3d9c2006a0223aec5
jueunkim205 <monolinum@naver.com>
2025-04-15 21:59:06 +0900

    update

--- js/script.js
+++ js/script.js
@@ -7,7 +7,32 @@ window.addEventListener('wheel', function (e) {
   // 아래 + , 위 -
   if (delta > 0) {
     header.style.transform = 'translateY(-200px)'
-  } else{
+  } else {
     header.style.transform = 'translateY(0)'
   }
 })
+
+
+// 숫자 카운터
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(function (counter) {
+  counter.textContent = "0";
+
+  const targetNum = +counter.getAttribute("data-target");
+
+  const updateCounter = () => {
+    const count = +counter.textContent;
+
+    const increment = targetNum / 100;
+    const nextCount = Math.ceil(count + increment);
+
+    counter.textContent = nextCount > targetNum ? targetNum : nextCount;
+
+    if (count < targetNum) {
+      requestAnimationFrame(updateCounter);
+    }
+  };
+
+  updateCounter();
+});
