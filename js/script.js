6e5fe2e27c70e82f9933518dfff2a347bc85b415
jueunkim205~ <ga473625@gmail.com>
2025-04-16 12:44:02 +0900

    update

--- js/script.js
+++ js/script.js
@@ -4,14 +4,19 @@ let header = document.querySelector("header");
 window.addEventListener("wheel", function (e) {
   let ht = document.documentElement.scrollTop;
   let delta = e.deltaY;
-  // console.log(ht);
+  console.log(ht);
   // console.log(delta);
   // 아래 + , 위 -
-  if (delta > 0) {
-    header.style.transform = "translateY(-200px)"; // 휠 아래로 내리면 가리기
-  } else if (delta < 0 && ht != 0) {
-    header.style.transform = "translateY(0)"; // 휠 위로 올리면 보이기
+
+  if (delta > 0 && ht != 0) { // 휠 아래로 내리면
+    header.style.transform = "translateY(-200px)"; 
+    header.classList.remove("on");
+  } else if (delta < 0 && ht > 500) {  // 휠 위로 올리면 보이기
+    header.style.transform = "translateY(0)";
     header.classList.add("on");
+  } else if (delta < 0 && ht <= 500) { // 휠 위로 올리는데 최상단에 가까워지면
+    header.style.transform = "translateY(0)";
+    header.classList.remove("on");
   } else {
     header.classList.remove("on");
   }
@@ -26,12 +31,14 @@ counters.forEach(function (counter) {
   const targetNum = +counter.getAttribute("data-target");
 
   const updateCounter = () => {
-    const count = +counter.textContent;
+    const count = +counter.textContent.replace(/,/g, ""); // 콤마제거 후 숫자로 변환
 
     const increment = targetNum / 100;
     const nextCount = Math.ceil(count + increment);
 
-    counter.textContent = nextCount > targetNum ? targetNum : nextCount;
+    const finalCount = nextCount > targetNum ? targetNum : nextCount;
+
+    counter.textContent = finalCount.toLocaleString(); // 쉼표처리
 
     if (count < targetNum) {
       requestAnimationFrame(updateCounter);
@@ -39,4 +46,11 @@ counters.forEach(function (counter) {
   };
 
   updateCounter();
+});
+
+AOS.init({
+  duration: 1000,
+  // delay: 500,
+  easing: "ease-in-out-sine",
+  offset: 500,
 });
