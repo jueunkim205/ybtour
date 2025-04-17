ee98cd31b49e9d12297ac8d8d4e8feb5acfd4d42
jueunkim205~ <ga473625@gmail.com>
2025-04-17 17:40:11 +0900

    update

--- js/subpage.js
+++ js/subpage.js
@@ -5,3 +5,14 @@ $(".section4 ul li")
   .on("mouseleave", function () {
     $(this).removeClass("on");
   });
+
+$("#sub2Btns a").eq(0).addClass("on");
+
+$("#sub2Btns a").on("click", function () {
+  if ($(this).hasClass("on")) {
+    $(this).removeClass("on");
+  } else {
+    $("#sub2Btns a").removeClass("on");
+    $(this).addClass("on");
+  }
+});
