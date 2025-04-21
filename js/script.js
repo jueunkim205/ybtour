ea2ac317868b55075c4bdca295e7bc588ba40fb4
jueunkim205~ <ga473625@gmail.com>
2025-04-21 09:56:35 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 // 새로고침시 최상단으로 이동
 // history.scrollRestoration = "manual";
 
-
 // header 메인메뉴
 let header = document.querySelector("header");
 
 window.addEventListener("wheel", function (e) {
   let ht = document.documentElement.scrollTop;
   let delta = e.deltaY;
-  console.log(ht);
+  // console.log(ht);
   // console.log(delta);
   // 아래 + , 위 -
 
-  if (delta > 0 && ht != 0) { // 휠 아래로 내리면
+  if (delta > 0 && ht != 0) {
+    // 휠 아래로 내리면
     header.style.transform = "translateY(-200px)";
     header.classList.remove("on");
-  } else if (delta < 0 && ht > 500) {  // 휠 위로 올리면 보이기
+  } else if (delta < 0 && ht > 500) {
+    // 휠 위로 올리면 보이기
     header.style.transform = "translateY(0)";
     header.classList.add("on");
-  } else if (delta < 0 && ht <= 500) { // 휠 위로 올리는데 최상단에 가까워지면
+  } else if (delta < 0 && ht <= 500) {
+    // 휠 위로 올리는데 최상단에 가까워지면
     header.style.transform = "translateY(0)";
     header.classList.remove("on");
   } else {
@@ -75,7 +77,7 @@ function startCounter(counter) {
 }
 
 function resetCounters() {
-  counters.forEach(counter => startCounter(counter));
+  counters.forEach((counter) => startCounter(counter));
 }
 
 let wasAtTop = false;
@@ -98,15 +100,48 @@ window.addEventListener("load", () => {
   }
 });
 
-
-
 // -----------------------------------
 
 AOS.init({
-  duration: 1000,
+  // duration: 800,
   // delay: 500,
-  easing: "ease-in-out-sine",
-  offset: 500,
+  // easing: "ease-in-out-sine",
+  // offset: 400,
 });
 
+// 반응형에서 다르게 주기
+
+  function updateAOS() {
+    const textboxes = document.querySelectorAll(".section3 li .textbox");
+    const pTag = document.querySelectorAll(".section3 li>p");
+
+
+    textboxes.forEach((textbox, k) => {
+      if (window.innerWidth < 768) {
+        textbox.setAttribute("data-aos-duration", "500");
+        textbox.setAttribute("data-aos-delay", "200");
+        textbox.setAttribute("data-aos-offset", "0");
+        pTag[k].setAttribute("data-aos-duration", "500");
+        pTag[k].setAttribute("data-aos-delay", "200");
+        pTag[k].setAttribute("data-aos-offset", "0");
+        // console.log(window.innerWidth);
+      } else {
+        textbox.setAttribute("data-aos-duration", "800");
+        textbox.setAttribute("data-aos-delay", "500");
+        textbox.setAttribute("data-aos-offset", "300");
+        pTag[k].setAttribute("data-aos-duration", "800");
+        pTag[k].setAttribute("data-aos-delay", "500");
+        pTag[k].setAttribute("data-aos-offset", "300");
+      }
+    });
+
+    AOS.refresh(); // 꼭 필요!
+  }
+
+  window.addEventListener("DOMContentLoaded", updateAOS);
+  window.addEventListener("resize", () => {
+    // resize 성능 최적화를 위해 debounce 사용 (선택 사항)
+    clearTimeout(window._aosResizeTimer);
+    window._aosResizeTimer = setTimeout(updateAOS, 150);
+  });
 
