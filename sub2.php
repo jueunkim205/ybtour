320760dcac9f55768cee192cc33d623fcd8af027
jueunkim205 <monolinum@naver.com>
2025-05-21 22:22:13 +0900

    update

--- sub2.php
+++ sub2.php
@@ -23,78 +23,19 @@
   <!-- aos -->
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
 
-  <link rel="stylesheet" href="./css/headerfooter.css" />
-  <link rel="stylesheet" href="./css/sub_common.css" />
-  <link rel="stylesheet" href="./css/sub2.css">
+  <link rel="stylesheet" href="./css/layout.css" />
+  <link rel="stylesheet" href="./css/sub.css">
 
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 </head>
 
 <body>
   <!-- https://company.ybtour.co.kr/invst/finanInfo -->
-  <div class="wrap">
-    <header>
-      <nav>
-        <a href="index.html" class="logo">
-          <img src="./images/logo.png" alt="logo"></a>
+  <div class="wrap sub2">
 
-        <ul class="mainMenu">
-          <li>
-            <a href="./sub1.html">회사소개</a>
-            <!-- submenu -->
-            <ul class="subMenu">
-              <li><a href="./sub1.html">회사소개</a></li>
-              <li><a href="#">대표인사말</a></li>
-              <li><a href="#">연혁</a></li>
-              <li><a href="#">고객만족경영</a></li>
-              <li><a href="#">윤리경영</a></li>
-              <li><a href="#">CI소개</a></li>
-              <li><a href="#">오시는길</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="./sub2.html">투자정보</a>
-            <ul class="subMenu">
-              <li><a href="./sub2.html">재무정보</a></li>
-              <li><a href="#">공고사항</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">대리점안내</a>
-            <ul class="subMenu">
-              <li><a href="#">대리점소개</a></li>
-              <li><a href="#">개설신청안내</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">홍보센터</a>
-            <ul class="subMenu">
-              <li><a href="#">사회공헌</a></li>
-              <li><a href="#">인증·수상</a></li>
-              <li><a href="#">보도기사</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">채용정보</a>
-            <ul class="subMenu">
-              <li><a href="#">인재상</a></li>
-              <li><a href="#">복지제도</a></li>
-              <li><a href="#">채용정보</a></li>
-            </ul>
-          </li>
-        </ul>
+    <?php include 'subhead.php'; ?>
 
-        <button class="mOnly">
-          <div></div>
-          <div></div>
-          <div></div>
-        </button>
-
-        <div class="bg"></div>
-      </nav>
-    </header>
-
-    <div class="subTitle" data-aos="fade-up">
+    <div class="subTitle">
       <div class="txt">
         <p>투자정보</p>
         <h1>재무정보</h1>
@@ -370,136 +311,12 @@
       </div>
     </section>
 
-    <div id="sideMenu">
-      <ul>
-        <li class="mainMenu"><a href="#">회사소개<span class="material-symbols-outlined">keyboard_arrow_down</span></a>
-          <ul class="subMenu">
-            <li><a href="./sub1.html">회사소개</a></li>
-            <li><a href="#">대표인사말</a></li>
-            <li><a href="#">연혁</a></li>
-            <li><a href="#">고객만족경영</a></li>
-            <li><a href="#">윤리경영</a></li>
-            <li><a href="#">CI소개</a></li>
-            <li><a href="#">오시는길</a></li>
-          </ul>
-        </li>
-        <li class="mainMenu"><a href="#">투자정보<span class="material-symbols-outlined">keyboard_arrow_down</span></a>
-          <ul class="subMenu">
-            <li><a href="./sub2.html">재무정보</a></li>
-            <li><a href="#">공고사항</a></li>
-          </ul>
-        </li>
-        <li class="mainMenu"><a href="#">대리점안내<span class="material-symbols-outlined">keyboard_arrow_down</span></a>
-          <ul class="subMenu">
-            <li><a href="#">대리점소개</a></li>
-            <li><a href="#">개설신청안내</a></li>
-          </ul>
-        </li>
-        <li class="mainMenu"><a href="#">홍보센터<span class="material-symbols-outlined">keyboard_arrow_down</span></a>
-          <ul class="subMenu">
-            <li><a href="#">사회공헌</a></li>
-            <li><a href="#">인증·수상</a></li>
-            <li><a href="#">보도기사</a></li>
-          </ul>
-        </li>
-        <li class="mainMenu"><a href="#">채용정보<span class="material-symbols-outlined">keyboard_arrow_down</span></a>
-          <ul class="subMenu">
-            <li><a href="#">인재상</a></li>
-            <li><a href="#">복지제도</a></li>
-            <li><a href="#">채용정보</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
 
-    <footer>
-      <div class="up">
-        <div class="banner">
-          <ul>
-            <li><a href="#">개인정보처리방침</a></li>
-            <li><a href="#">이용약관</a></li>
-            <li><a href="#">여행약관</a></li>
-            <li><a href="#">여행자보험</a></li>
-            <li><a href="#">대리점 소개</a></li>
-            <li><a href="#">오시는 길</a></li>
-            <li><a href="#">노랑풍선</a></li>
-            <li><a href="#">고객센터</a></li>
-          </ul>
-
-          <ul>
-            <li>
-              <a href="https://www.facebook.com/yellowballoonofficial/">
-                <p></p>
-              </a>
-            </li>
-            <li>
-              <a href="https://pf.kakao.com/_hhAxbd">
-                <p></p>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.youtube.com/user/ybtour2013">
-                <p></p>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.instagram.com/yellowballoon_official/">
-                <p></p>
-              </a>
-            </li>
-            <li>
-              <a href="https://blog.naver.com/yb_tour">
-                <p></p>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="down">
-        <div class="con">
-          <div class="top">
-            <p>(주) 노랑풍선<img src="./images/logo_kosdaq.png" alt="코스닥상장법인"></p>
-            <span class="material-symbols-outlined mOnly">
-              arrow_forward_ios
-            </span>
-          </div>
-
-          <div class="detail">
-            <ul class="left">
-              <li><span>대표이사 김진국</span><span>서울특별시 중구 수표로 31, 노랑풍선빌딩</span></li>
-              <li><span>사업자등록번호 104-81-64440</span><span>관광사업자등록증번호
-                  제2006-000003호</span><span>통신판매업신고번호 제2008-서울중구-0278 <a href="">사업자 정보확인</a></span>
-              </li>
-              <li><span>[부산지사] 부산광역시 부산진구 중앙대로668, 9층 912호 (부전동, 에이원프라자)</span><span>사업자등록번호
-                  870-85-01900</span></li>
-              <li>
-                <p>
-                  영업배상책임보험가입 총 20억원 · 한국관광협회중앙회 여행공제회 공제영업보증가입 10억원 · 공제기획여행보증가입
-                  5억원
-                </p>
-              </li>
-            </ul>
-
-            <ul class="right">
-              <li><span>대표메일</span><a href="mailto:master@ybtour.com">master@ybtour.com</a></li>
-              <li><span>마케팅제휴</span><a href="mailto:marketing@ybtour.com">marketing@ybtour.com</a></li>
-              <li><span>판매제휴</span><a href="mailto:salespartner@ybtour.com">salespartner@ybtour.com</a></li>
-              <li><span>대리점문의</span><a href="mailto:sales.agent@ybtour.co.kr">sales.agent@ybtour.co.kr</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="award">
-          <a href=""><img src="./images/awards.png" alt=""></a>
-        </div>
-        <p class="copy">©Yellow Balloon tour. All Rights Reserved.</p>
-      </div>
-
-    </footer>
   </div>
 
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-  <script src="./js/script.js"></script>
+  <?php include 'subtail.php'; ?>
   <script src="./js/subpage.js"></script>
+
 </body>
 
 </html>
